55e93085b32cd34361537ea20a2e0cc05253e674
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-24 17:22:19 +0000

    Destroy model Nortification

--- app/models/nortification.rb
+++
@@ -1,2 +0,0 @@
-class Nortification < ApplicationRecord
-end
--- db/migrate/20221015055821_create_nortifications.rb
+++
@@ -1,8 +0,0 @@
-class CreateNortifications < ActiveRecord::Migration[6.1]
-  def change
-    create_table :nortifications do |t|
-
-      t.timestamps
-    end
-  end
-end
