4c079ec27ebb79fd450910a176659767678fdf90
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 09:50:44 +0000

    Edit admin/japan_areas controller

--- app/controllers/admin/japan_areas_controller.rb
+++ app/controllers/admin/japan_areas_controller.rb
@@ -1,7 +1,24 @@
 class Admin::JapanAreasController < ApplicationController
-  def edit
-  end
+
+  before_action :authenticate_admin!
 
   def index
   end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def japan_area_params
+    params.require(:japan_area).permit(:name)
+  end
+
 end
