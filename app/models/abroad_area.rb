d674a9be57b385e0c439df39e8198115e703be91
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 00:55:02 +0000

    Destroy model Abroad_area

--- app/models/abroad_area.rb
+++
@@ -1,2 +0,0 @@
-class AbroadArea < ApplicationRecord
-end
--- db/migrate/20221015062138_create_abroad_areas.rb
+++
@@ -1,8 +0,0 @@
-class CreateAbroadAreas < ActiveRecord::Migration[6.1]
-  def change
-    create_table :abroad_areas do |t|
-
-      t.timestamps
-    end
-  end
-end
