ca66e38a63185c199c83b6151c7755d97154bf90
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 01:54:56 +0000

    Add columns to articles

--- db/migrate/20221015055959_create_articles.rb
+++ db/migrate/20221015055959_create_articles.rb
@@ -1,8 +1,17 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
+      t.string :region
+      t.string :name, null: false
+      t.string :title, null: false
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.text :body, null: false
+      t.float :rate
+      t.boolean :is_active, null: false, default: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
