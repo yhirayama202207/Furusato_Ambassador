164ab3246b58e9306b492d05d1cd683d328a41e7
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 02:09:50 +0000

    Add columns to japan_prefectures

--- db/migrate/20221015061441_create_japan_prefectures.rb
+++ db/migrate/20221015061441_create_japan_prefectures.rb
@@ -1,8 +1,9 @@
 class CreateJapanPrefectures < ActiveRecord::Migration[6.1]
   def change
     create_table :japan_prefectures do |t|
+      t.string :name, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
