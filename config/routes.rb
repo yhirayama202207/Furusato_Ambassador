451c087227dc5178f6df8e038b8591fa4b8adfe5
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 04:37:03 +0000

    Edit routings devise for admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
   # 顧客用
   # URL /customers/sign_in ...
-  devise_for :users, controllers: {
+  devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
