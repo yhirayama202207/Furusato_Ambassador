e9cab5951f6e5b820e62e9f759c18daa3e3588ae
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 10:09:36 +0000

    Edit admin/japan_areas controller

--- app/controllers/admin/japan_areas_controller.rb
+++ app/controllers/admin/japan_areas_controller.rb
@@ -3,15 +3,30 @@ class Admin::JapanAreasController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @japan_area = JapanArea.new
+    @japan_areas = JapanArea.all
   end
 
   def create
+    @japan_area = JapanArea.new(japan_area_params)
+    if @japan_area.save
+      flash[:notice] = "日本エリア登録が完了しました"
+      redirect_to admin_japan_areas_path
+    end
   end
 
   def edit
+    @japan_area = JapanArea.find(params[:id])
   end
 
   def update
+    @japan_area = JapanArea.find(params[:id])
+    if @japan_area.update(japan_area_params)
+      flash[:notice] = "ジャンル名変更が完了しました"
+      redirect_to admin_japan_areas_path
+    else
+      render :edit
+    end
   end
 
   #投稿データのストロングパラメータ
