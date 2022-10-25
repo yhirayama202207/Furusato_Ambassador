ad5d8d3004c62f42304509c0d82e605d507e5d1e
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 01:35:06 +0000

    Add column to cheer_regions

--- db/migrate/20221025011536_create_cheer_regions.rb
+++ db/migrate/20221025011536_create_cheer_regions.rb
@@ -1,8 +1,9 @@
 class CreateCheerRegions < ActiveRecord::Migration[6.1]
   def change
     create_table :cheer_regions do |t|
+      t.string :name
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
