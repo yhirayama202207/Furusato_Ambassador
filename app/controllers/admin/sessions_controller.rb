042ef64b8d54ba521f4119f45f82069e1687e82f
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 04:19:30 +0000

    Edit admin/sessions_controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,8 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :configure_permitted_parameters , if: :devise_controller?
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,10 +20,22 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def configure_permitted_parameters
+   	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email, :encrypted_password])
+  end
 end
