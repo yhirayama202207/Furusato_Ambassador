17046438720e9f91bb3519de1bf914d982057908
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-25 02:02:08 +0000

    Add columns to comments

--- db/migrate/20221015060126_create_comments.rb
+++ db/migrate/20221015060126_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.text :comment, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
