88fae610065fc2364f798baab63c01424706965d
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-29 11:43:04 +0000

    Edit controller admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,32 @@
 class Admin::UsersController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def index
+    @users = User.all.page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "会員情報の更新が完了しました"
+      redirect_to admin_user_path(@user.id)
+    end
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :home, :hometown, :is_deleted)
+  end
+
 end
