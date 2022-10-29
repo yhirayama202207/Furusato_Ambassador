8246a721396dfc211a2b81ac8f7e2acac28a5a9a
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-29 11:18:08 +0000

    Generated controller&views admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
   namespace :public do
     get 'users/show'
     get 'users/edit'
