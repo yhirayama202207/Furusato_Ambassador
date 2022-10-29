05c399c5ed4a742391cb390a38a1646cad9b461d
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-10-29 11:12:46 +0000

    Add routing public/users

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     get "homes/about"
     get "users/confirm" => "public/users#confirm"
     patch "users/unsubscribe" => "public/users#unsubscribe"
-    resources :users, only: [:show, :edit, :update]
+    resources :users, only: [:show, :edit, :update, :index]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
