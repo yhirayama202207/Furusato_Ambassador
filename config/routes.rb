b1813e726fcd6a60cd8919fbd766a01ca3102817
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-03 15:56:46 +0000

    Generated controller&views admin/articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'articles/index'
+    get 'articles/show'
+    get 'articles/edit'
+    get 'articles/update'
+    get 'articles/destroy'
+  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
