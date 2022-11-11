bf3d62b1e565776f97c06e5923217c7aaecbb65d
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-11 07:50:16 +0000

    Edit controller admin/articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,16 +1,25 @@
 class Admin::ArticlesController < ApplicationController
+
   def index
+    @articles = Article.where(is_active: true)
+    @japan_prefectures = JapanPrefecture.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
+    @article = Article.find(params[:id])
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to admin_articles_path
   end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def article_params
+    params.require(:article).permit(:user_id, :japan_prefecture_id, :region, :name, :title, :address, :latitude, :longitude, :body, :rate, :is_active, :image)
+  end
+
 end
