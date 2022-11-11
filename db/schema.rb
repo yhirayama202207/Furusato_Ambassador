6380c3d4fc4f7716c988455e6e32f767528a1cf6
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-11 12:23:01 +0000

    Add columns to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 2022_10_25_120022) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
     t.text "comment", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
