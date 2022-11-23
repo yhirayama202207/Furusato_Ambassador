235a9144411785a68d2a7e03f6d057de45c4c4a3
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-23 12:02:05 +0000

    Edit view public/users/index

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,33 @@
+class Public::UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_articles = @user.articles.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "会員情報の更新が完了しました"
+      redirect_to user_path(@user.id)
+    end
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :home, :hometown, :is_deleted, :image)
+  end
+
+end
