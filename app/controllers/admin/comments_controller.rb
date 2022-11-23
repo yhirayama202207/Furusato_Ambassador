b99876bf741b77a5a649e888c73f876557af738a
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-23 16:27:00 +0000

    Edit admin/comments/index

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,4 +1,18 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to admin_comment_path
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :article_id, :comment, :image)
   end
 end
