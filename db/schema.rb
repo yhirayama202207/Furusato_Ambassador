b76ffd6b347b21b1966fcc2693b966dfe83e97fc
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-23 15:06:54 +0000

    Edit table relationships

--- db/schema.rb
+++ db/schema.rb
@@ -122,11 +122,6 @@ ActiveRecord::Schema.define(version: 2022_10_25_120022) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
