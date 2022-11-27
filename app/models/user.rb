cab9fa57b356fe895c94a5a2a3b9906a75d7c403
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-27 05:34:11 +0000

    Add function follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
 
-  # フォローをした、されたの関係
+  # フォローする
   has_many :relationships, foreign_key: :following_id, dependent: :destroy
+  #フォロー一覧用
   has_many :followings, through: :relationships, source: :follower
 
+  #フォローされる
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  #フォロワー一覧用
   has_many :followers, through: :reverse_of_relationships, source: :following
 
   # フォローしたときの処理
