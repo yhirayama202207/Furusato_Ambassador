41daf49b04a1b61d2fdc8c355aa0347a9285d1b6
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-27 13:06:33 +0000

    Add function foot_prints

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,14 @@ class Article < ApplicationRecord
   belongs_to :japan_prefecture
   has_many :comments
   has_many :likes, dependent: :destroy
-  
+  has_many :foot_prints, dependent: :destroy
+
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
+
+  def foot_printed_by?(user)
+    foot_prints.exists?(user_id: user.id)
+  end
+
 end
