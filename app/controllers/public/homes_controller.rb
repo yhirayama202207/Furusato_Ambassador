9c2d6a3e9c07b082adee50db68f06201f3440fa8
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-11-29 14:02:31 +0000

    Edit public/homes/top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    @articles = Article.where(is_active: true)
+    @japan_prefectures = JapanPrefecture.all
+    @all_ranks = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
   end
 
   def about
