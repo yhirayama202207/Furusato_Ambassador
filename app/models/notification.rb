f838873b5cd20ef5c064055775bd8184f689674a
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-12-03 12:31:21 +0000

    Add function notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,8 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+  belongs_to :article, optional: true
+  belongs_to :comment, optional: true
+
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id", optional: true
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id", optional: true
 end
