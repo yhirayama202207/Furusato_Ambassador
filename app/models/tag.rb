aa25ba5a5eaa6dd85a5ab691d8e5e76c59a15bc6
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-12-11 11:46:34 +0000

    Add function Tag

---
+++ app/models/tag.rb
@@ -0,0 +1,9 @@
+class Tag < ApplicationRecord
+  # tag削除時、中間テーブルも削除
+  has_many :article_tags, dependent: :destroy
+  has_many :articles, through: :article_tags
+  # 空白登録はNG
+  validates :name, presence: true
+  # 重複登録NG
+  validates :name, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_122628) do
+ActiveRecord::Schema.define(version: 2022_12_11_113517) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 2022_12_03_122628) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "article_tags", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "articles", force: :cascade do |t|
@@ -163,6 +170,12 @@ ActiveRecord::Schema.define(version: 2022_12_03_122628) do
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id", null: false
     t.integer "following_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
