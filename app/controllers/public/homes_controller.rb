df3300fb8b4d06c2597fe3fb3d06fb224522f93a
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-12-14 15:55:02 +0000

    layout adjustment

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,7 @@ class Public::HomesController < ApplicationController
     @articles = Article.where(is_active: true)
     @japan_areas = JapanArea.all
     @japan_prefectures = JapanPrefecture.all
-    @all_ranks = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+    @all_ranks = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(4).pluck(:article_id))
   end
 
   def about
