b5e81dde0449c00767c2251efa214c3a0731f3c9
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-12-15 05:02:33 +0000

    layout adjustment

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,8 @@
 class Public::NotificationsController < ApplicationController
 
   def index
+    @japan_areas = JapanArea.all
+    @japan_prefectures = JapanPrefecture.all
     #current_userの投稿に紐づいた通知一覧
     @notifications = current_user.passive_notifications.where.not(sender_id: current_user).page(params[:page]).per(20)
     #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
