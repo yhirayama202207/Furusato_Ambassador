82d6fd1e583acc7837a28af4a76c922796b534a6
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2023-01-15 12:08:37 +0000

    Deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  email: ENV['LOGIN_MAIL'],
-  password: ENV['LOGIN_PASSWORD']
-)
+# Admin.create!(
+#   email: ENV['LOGIN_MAIL'],
+#   password: ENV['LOGIN_PASSWORD']
+# )
+
+# 管理者登録
+admins = [
+  {email: ENV['LOGIN_MAIL'], password: ENV['LOGIN_PASSWORD']}
+]
+
+admins.each do |admin|
+  admin_data = Admin.find_by(admin[:email])
+  if admin_data.nil?
+    Admin.create(email: admin[:email], password: [:password])
+  end
+end
+
+# エリア
+# areas = [
+#   {name: '北海道', english_name: 'Hokkaido'},
+#   {name: '北海道', english_name: 'Hokkaido'},
+# ]
+
+# areas.each do |area|
+#   Area.find_or_create_by(name: area[:name], english_name: area[:english_name])
+# end
